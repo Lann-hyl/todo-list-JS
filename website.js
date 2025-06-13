ae36ba8b493921c5532d02606711f46157ab3644
Lann <hu.julien.ts4@gmail.com>
2025-06-13 21:27:56 +0200

    add dropdown menu for todo items

--- website.js
+++ website.js
@@ -5,6 +5,17 @@ const todoListUl = document.getElementById('todo-list')
 let todoList = loadTodos(); 
 updateTodoList();
 
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches(".dropdown-button");
+    const isDropdownMenu = e.target.closest(".dropdown");
+    if (!isDropdownButton && !isDropdownMenu) {
+        document.querySelectorAll(".dropdown.active").forEach(dropdown => {
+            dropdown.classList.remove('active');
+        });
+    }
+})
+
 todoForm.addEventListener('submit', function(e) {
     e.preventDefault();
     addTodo();
@@ -43,21 +54,40 @@ function createTodoItem(todo, todoIndex) {
         <label for="${todoID}" class="todo-text">
             ${todo.text}
         </label>
-        <button class="delete-button">
-            <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-        </button>
-    `
-    const deleteButton = todoLI.querySelector(".delete-button");
-    deleteButton.addEventListener("click", () => {
-        deleteTodoItem(todoIndex);
-    });
 
+        <div class="dropdown">
+            <button class="dropdown-button"> <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z"/></svg> </button>
+            <div class="dropdown-menu">
+                <button class="delete-button"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                    Delete 
+                </button>
+            </div>
+        </div>
+    `
+    
     const checkbox = todoLI.querySelector("input");
     checkbox.addEventListener("change", () => {
         todoList[todoIndex].completed = checkbox.checked;
         saveTodos();
     });
     checkbox.checked = todo.completed;
+
+    const dropdownButton = todoLI.querySelector(".dropdown-button");
+    dropdownButton.addEventListener("click", () => {
+        let currentDropdown = dropdownButton.closest(".dropdown");
+        currentDropdown.classList.toggle('active');
+
+        document.querySelectorAll(".dropdown.active").forEach(dropdown => {
+            if (dropdown !== currentDropdown) dropdown.classList.remove('active');
+        });
+    });
+
+    const deleteButton = todoLI.querySelector(".delete-button");
+    deleteButton.addEventListener("click", () => {
+        deleteTodoItem(todoIndex);
+    });
+
     return todoLI;
 }
 
