7181e37f600ce55de9eee1c8591af319e20b5b6d
Lann <43467955+Lann-hyl@users.noreply.github.com>
2025-09-12 20:03:43 +0200

    add description toggle button

--- website.js
+++ website.js
@@ -63,6 +63,8 @@ function addTodo() {
             deadlineDate: new Date(),
             deadlineTime: new Date(),
             displayDeadline: false,
+            displayDescription: false,
+            description: "",
             text: todoText,
             completed: false
         }
@@ -84,6 +86,10 @@ function updateTodoList() {
         }
         if (!('displayDeadline' in todo)) {
             todo.displayDeadline = false;
+        }
+        if (!('displayDescription' in todo)) {
+            todo.displayDescription = false;
+            todo.description = "";
         }
         if (!('deadlineDate' in todo)) {
             todo.deadlineDate = new Date();
@@ -136,6 +142,10 @@ function createTodoItem(todo, todoIndex) {
                 <button class="deadlineToggle-button"> 
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M680-80v-120H560v-80h120v-120h80v120h120v80H760v120h-80Zm-480-80q-33 0-56.5-23.5T120-240v-480q0-33 23.5-56.5T200-800h40v-80h80v80h240v-80h80v80h40q33 0 56.5 23.5T760-720v244q-20-3-40-3t-40 3v-84H200v320h280q0 20 3 40t11 40H200Zm0-480h480v-80H200v80Zm0 0v-80 80Z"/></svg>
                     ${todo.displayDeadline ? "Hide deadline" : "Show Deadline"}
+                </button>
+                <button class="descriptionToggle-button"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>
+                    ${todo.displayDescription ? "Hide description" : "Show description"}
                 </button>
                 <button class="quantityToggle-button"> 
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Zm-20 200h80v-400H380v80h80v320Z"/></svg>
